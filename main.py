7229cb0accc205141009c2f46980582341de8025
James-Lu-none <jamesabcde277@gmail.com>
2025-01-04 01:32:04 +0800

    improve logging format in TelloDrone to ensure consistent data types

--- main.py
+++ main.py
@@ -173,7 +173,11 @@ class TelloDrone(Tello):
                     cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
                     with open(self.log_file_path, "a") as log_file:
-                        log_file.write(f"Time: {pTime};bat: {str(self.get_battery())};{str([x_center, y_center, width, height])};{str([ud_dif, fb_dif, yv_dif])};{str([self.lr, self.fb, self.ud, self.yv])}\n")
+                        log_file.write(
+                            f"Time: {pTime};bat: {str(self.get_battery())};
+                            {str([float(x_center), float(y_center), float(width), float(height)])};
+                            {str([float(ud_dif), float(fb_dif), float(yv_dif)])};
+                            {str([self.lr, self.fb, self.ud, self.yv])}\n")
                 else:
                     # self.lr, self.fb, self.ud, self.yv = 0,0,0,0
                     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
