d6f7ecdb88b31320f1a315021eaaf316e24cd63f
Tucker5921 <peter31101@gmail.com>
2025-01-10 13:23:40 +0800

    update control mechanisms

--- main.py
+++ main.py
@@ -37,9 +37,9 @@ class TelloDrone(Tello):
         self.rev_speed = 0
         self.keyboard_thread = Thread(target=self.getKeyboardInput)
         self.keyboard_thread.start()
-        self.pid_fb = PID(1, 0.1, 0.3, setpoint=0)
-        self.pid_ud = PID(0.11, 0.001, 0.1, setpoint=0)
-        self.pid_yv = PID(0.10, 0.01, 0.07, setpoint=0)
+        self.pid_fb = PID(1.4, 0.05, 0.3, setpoint=0)
+        self.pid_ud = PID(0.145, 0.001, 0.06, setpoint=0)
+        self.pid_yv = PID(0.127, 0.005, 0.06, setpoint=0)
         self.manual_flag = False
         self.update_width_flag = False
         self.target_width = 0
@@ -78,6 +78,8 @@ class TelloDrone(Tello):
             elif keyboard.is_pressed("DOWN"): 
                 self.fb = -speed
                 self.update_width_flag = True
+            else:
+                self.update_width_flag = False
             
             if keyboard.is_pressed("w"): self.ud = speed
             elif keyboard.is_pressed("s"): self.ud = -speed
@@ -85,22 +87,32 @@ class TelloDrone(Tello):
             if keyboard.is_pressed("a"): self.yv = -speed
             elif keyboard.is_pressed("d"): self.yv = speed
             
+
             if keyboard.is_pressed("9"): 
                 self.land()
                 break
             
             if keyboard.is_pressed("e"): self.takeoff()
 
+            hold1 = 0
+            hold2 = 0
             if keyboard.is_pressed("1") and self.rev_speed > -100:
-                self.rev_speed-=step
-                print("revolution speed: ", self.rev_speed)
+                if hold1 == 0:
+                    self.rev_speed-=step
+                    hold1 = 1
+                    print("revolution speed: ", self.rev_speed)
+            else:   hold1 = 0
             
             if keyboard.is_pressed("2") and self.rev_speed < 100:
-                self.rev_speed+=step
-                print("revolution speed: ", self.rev_speed)
+                if hold2 == 0:
+                    self.rev_speed+=step
+                    hold2 = 1
+                    print("revolution speed: ", self.rev_speed)
+            else:   hold2 = 0
             
             if keyboard.is_pressed("space") and self.space_state == 0:
                 self.space_state = 1
+                self.rev_speed = 10
             if not keyboard.is_pressed("space") and self.space_state == 1:
                 self.space_state = 0
                 self.rev_on = not self.rev_on
@@ -160,69 +172,66 @@ class TelloDrone(Tello):
             prev_time = time.time()
             if(len(periods) == period_window_size): 
                 fps = 1 / (sum(periods) / len(periods))
-                periods.clear()
-
-            if not self.manual_flag:
-                self.detections = results.xywh[0].cpu().numpy()
-                for det in self.detections:
-                    x_center, y_center, width, height, confidence, class_id = det
-                    x1 = int(x_center - width / 2)
-                    y1 = int(y_center - height / 2)
-                    x2 = int(x_center + width / 2)
-                    y2 = int(y_center + height / 2)
-                    label = f"{results.names[int(class_id)]} {confidence:.2f}"
-                    if(class_id == self.lock_class_id):
-                        if(self.update_width_flag):
-                            self.target_width = width
-                            self.update_width_flag = False
-                        ud_dif = y_center - self.frame_center[1]
-                        fb_dif = width - self.target_width
-                        yv_dif = x_center - self.frame_center[0]
+                
+            self.detections = results.xywh[0].cpu().numpy()
+            for det in self.detections:
+                x_center, y_center, width, height, confidence, class_id = det
+                x1 = int(x_center - width / 2)
+                y1 = int(y_center - height / 2)
+                x2 = int(x_center + width / 2)
+                y2 = int(y_center + height / 2)
+                label = f"{results.names[int(class_id)]} {confidence:.2f}"
+                if(class_id == self.lock_class_id):
+                    if keyboard.is_pressed("t"): self.target_width = width
+                    ud_dif = y_center - self.frame_center[1]
+                    fb_dif = width - self.target_width
+                    yv_dif = x_center - self.frame_center[0]
+                    if(not self.manual_flag):
                         self.adj_pose(ud_dif, fb_dif, yv_dif)
-                        # print(self.update_width_flag, self.target_width, width, fb_dif)
-                        if(self.fb>0):
-                            cv2.circle(image, self.frame_center, self.fb, (0, 255, 255), 2)
-                        else:
-                            cv2.circle(image, self.frame_center, self.fb*-1, (0, 0, 255), 2)
-                        cv2.arrowedLine(image, self.frame_center, (self.frame_center[0],self.frame_center[1]-self.ud), (0, 255, 255), 2)
-                        cv2.arrowedLine(image, self.frame_center, (self.frame_center[0]+self.yv,self.frame_center[1]), (0, 255, 255), 2)
-
-                        cv2.circle(image, (int(x_center), int(y_center)), 2, (255, 0, 0), 2)
-                        cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                        cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-                        bounding_box = [
-                            round(float(x_center), 3),
-                            round(float(y_center), 3),
-                            round(float(width), 3),
-                            round(float(height), 3)
-                        ]
-                        differences = [
-                            round(float(ud_dif), 3),
-                            round(float(fb_dif), 3),
-                            round(float(yv_dif), 3)
-                        ]
-                        with open(self.log_file_path, "a") as log_file:
-                            log_message = (
-                                f"Time: {prev_time};"
-                                f"bat: {str(self.get_battery())};"
-                                f"{str(bounding_box)};"
-                                f"{str(differences)};"
-                                f"{str([self.lr, self.fb, self.ud, self.yv])}\n"
-                            )
-                            log_file.write(log_message)
+                    # print(self.update_width_flag, self.target_width, width, fb_dif)
+                    if(self.fb>0):
+                        cv2.circle(image, self.frame_center, self.fb, (0, 255, 255), 2)
                     else:
-                        # self.lr, self.fb, self.ud, self.yv = 0,0,0,0
-                        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                        cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                cv2.circle(image, self.frame_center, 2, (255, 255, 255), 2)
+                        cv2.circle(image, self.frame_center, self.fb*-1, (0, 0, 255), 2)
+                    cv2.arrowedLine(image, self.frame_center, (self.frame_center[0],self.frame_center[1]-self.ud), (0, 255, 255), 2)
+                    cv2.arrowedLine(image, self.frame_center, (self.frame_center[0]+self.yv,self.frame_center[1]), (0, 255, 255), 2)
+
+                    cv2.circle(image, (int(x_center), int(y_center)), 2, (255, 0, 0), 2)
+                    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                    cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+                    bounding_box = [
+                        round(float(x_center), 3),
+                        round(float(y_center), 3),
+                        round(float(width), 3),
+                        round(float(height), 3)
+                    ]
+                    differences = [
+                        round(float(ud_dif), 3),
+                        round(float(fb_dif), 3),
+                        round(float(yv_dif), 3)
+                    ]
+                    with open(self.log_file_path, "a") as log_file:
+                        log_message = (
+                            f"Time: {prev_time};"
+                            f"bat: {str(self.get_battery())};"
+                            f"{str(bounding_box)};"
+                            f"{str(differences)};"
+                            f"{str([self.lr, self.fb, self.ud, self.yv])}\n"
+                        )
+                        log_file.write(log_message)
+                else:
+                    # self.lr, self.fb, self.ud, self.yv = 0,0,0,0
+                    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.circle(image, self.frame_center, 2, (255, 255, 255), 2)
             cv2.putText(image, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.imshow('Detection Result', image)
             self.send_rc_control(self.lr, self.fb, self.ud, self.yv)
             # A argument "self" was added to mouse_callback function so parameter has to be None
             # otherwise it will be count as a positional argument and cause TypeError
             cv2.setMouseCallback('Detection Result', self.mouse_callback, param=None) 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord('9'):
                 break
 
         self.streamoff()
